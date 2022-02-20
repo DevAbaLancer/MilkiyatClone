cfd5475cd4cb5d2037a9ef9147a9c98e7b38e815
DevAbaLancer <94047358+DevAbaLancer@users.noreply.github.com>
2022-02-21 00:09:22 +0530

    minor_changes

--- lib/service/api_client.dart
+++ lib/service/api_client.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import '../Models/model.dart';
 import 'package:http/http.dart' as http;
@@ -7,15 +8,18 @@ class ApiClient {
   Model practice = Model();
 
   Future<Model> getPostApi() async {
-    final response =
-        await http.get(Uri.parse("https://milkiyat.bangalore2.com/api/home/"));
-    var data = jsonDecode(response.body.toString());
-    if (response.statusCode == 200) {
-      practice = Model.fromJson(data);
-
-      return practice;
-    } else {
-      return practice;
+    try {
+      final response = await http
+          .get(Uri.parse("https://milkiyat.bangalore2.com/api/home/"));
+      var data = jsonDecode(response.body.toString());
+      if (response.statusCode == 200) {
+        practice = Model.fromJson(data);
+      } else {
+        log('something went wrong');
+      }
+    } catch (e) {
+      log(e.toString());
     }
+    return practice;
   }
 }
